740254845a85a08319abe005597ae7aa58fe368b
marc-schumann <marc.andre.schumann@gmail.com>
2016-02-11 19:38:24 +0100

    Add: Unit-Tests / Travis-Ci

---
+++ src/MarcSchumann/YamlPathLoader/Tests/YamlPathLoaderTest.php
@@ -0,0 +1,28 @@
+<?php
+
+class YamlPathLoaderTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test - Setup
+     */
+    public function setUp()
+    {
+
+    }
+
+    /**
+     * Teardown Test
+     */
+    public function tearDown()
+    {
+
+    }
+
+    /**
+     * Tests YamlPathLoader::loadResource
+     */
+    public function testLoadResource()
+    {
+        $this->assertEquals(true, true);
+    }
+}
